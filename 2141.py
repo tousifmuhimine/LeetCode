9c0cacae103d1e486b5a14003328e98561a36c18
tousifmuhimine <abdullahtsn13@gmail.com>
2025-12-01 22:11:32 +0600

    2141

---
+++ 2141.py
@@ -0,0 +1,17 @@
+# 2141. Maximum Running Time of N Computers
+from typing import List
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        batteries.sort()
+        extra = sum(batteries[:-n])
+
+        live = batteries[-n:]
+
+        for i in range(n-1):
+            if extra // (i+1) < live[i+1] - live[i]:
+                return live[i]+extra // (i+1)
+            
+            extra -= (i+1)*(live[i+1]-live[i])
+
+        return live[-1] + extra // n
